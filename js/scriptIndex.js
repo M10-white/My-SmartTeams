70593abf38315abd5ef2100d30d8ecf47434bfa4
Brahim Chaouki <bchaouki00@gmail.com>
2024-09-27 01:28:01 +0200

    last

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -2,5 +2,5 @@ function toggleMenu() {
     var menu = document.getElementById("menu");
     var body = document.body; // Récupère le corps du document
     menu.classList.toggle("open");
-    body.classList.toggle("blur"); // Ajoute ou enlève le flou
+    body.classList.toggle("blur");
 }
